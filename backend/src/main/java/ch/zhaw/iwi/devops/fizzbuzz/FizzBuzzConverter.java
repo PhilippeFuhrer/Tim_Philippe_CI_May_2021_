fad267aa0d9ab3af1e5e2e88da698543a68b4596
PhilippeFuhrer <71648397+PhilippeFuhrer@users.noreply.github.com>
2021-04-26 13:58:19 +0200

    commit 26.04.2021

--- backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/fizzbuzz/FizzBuzzConverter.java
@@ -3,7 +3,7 @@ package ch.zhaw.iwi.devops.fizzbuzz;
 public class FizzBuzzConverter {
 
     public String convert(int i) {
-        if (i % 3 == 0 && i % 7 == 0) {
+        if (i % 99 == 0 && i % 7 == 0) {
             return "FizzBuzz";
         } else if (i % 3 == 0) {
             return "Fizz";
